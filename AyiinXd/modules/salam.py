4fcc43bc67a3146ea04fb20f04b6a999bb58b55c
ferdihardiyansa <85765224+ferdihardiyansa@users.noreply.github.com>
2022-11-01 02:45:02 +0800

    salam.py

--- AyiinXd/modules/salam.py
+++ AyiinXd/modules/salam.py
@@ -71,7 +71,7 @@ async def _(event):
     me = await event.client.get_me()
     xx = await edit_or_reply(event, f"**begitu syulitt lupakan {me.first_name}**")
     sleep(2.5)
-    await xx.edit("**apalagi {me.first_name} baik🥰**")
+    await xx.edit("**apalagi {me.first_name}baik**")
 
 
 CMD_HELP.update(
